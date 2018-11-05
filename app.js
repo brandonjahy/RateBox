6ba05c3cadcd99af8279c08766a8b7d1c2fd0cbb
Brandon-Johnson <b.johnss97@gmail.com>
2018-11-05 11:51:55 -0800

    functionality JS list

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 //for RateBox commit
+//Use Guidebox API with Ajax and JSON 
+//have a search box that will respond off of input
+//add a Like and Dislike feature
+//add a Modal for when image of item is clicked
